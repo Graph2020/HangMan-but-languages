d9b6cfa186366d26b0e74cdfd89b9148e19df47f
Jacopo <163671349+Graph2020@users.noreply.github.com>
2026-03-29 20:56:36 +0300

    add languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import Win from "./components/Win";
+import { languages } from "./languages";
 function App() {
-  return <div></div>;
+  const displayLanguages = languages.map(({ name, backgroundColor, color }) => (
+    <div
+      style={{ backgroundColor: backgroundColor, color: color }}
+      className="w-fit rounded-sm p-1"
+    >
+      {name}
+    </div>
+  ));
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-5 bg-[#282726] px-5 py-10 text-center">
+      <div>
+        <h1 className="font-main text-xl font-medium text-[#F9F4DA]">
+          Assembly: Endgame
+        </h1>
+        <p className="text-[#8E8E8E]">
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </div>
+      <Win />
+      <div className="flex max-w-96 flex-wrap items-center justify-center gap-1">
+        {displayLanguages}
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Win.jsx
@@ -0,0 +1,10 @@
+function Win() {
+  return (
+    <div className="flex w-full flex-col rounded-md bg-[#10A95B] text-white sm:w-96 sm:px-6">
+      <h3 className="text-lg">You Win!</h3>
+      <p className="text-md -mt-1">Well done! 🎉</p>
+    </div>
+  );
+}
+
+export default Win;
