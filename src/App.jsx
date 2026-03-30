e687584eee7fbe03f0e3b7a2abb818dabe84b33a
Jacopo <163671349+Graph2020@users.noreply.github.com>
2026-03-30 15:59:10 +0300

    add style to guessed keyboards, add logic to lose langauges

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,64 @@
 import { useState } from "react";
 import Win from "./components/Win";
 import { languages } from "./languages";
+import clsx from "clsx";
 function App() {
+  // States
   const [currentWord, setCurrentWord] = useState("react");
+  const [usersLetters, setUsersLetters] = useState([]);
 
+  // Derived
+
+  const wrongGuessCount = usersLetters.filter(
+    (letter) => !currentWord.includes(letter),
+  ).length;
+
+  // Functions
+  function addLetter(letter) {
+    setUsersLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter],
+    );
+  }
+
+  // Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-  const keyboardButtons = alphabet.map((keyboard) => (
-    <button className="bg-keyboard size-8 rounded-sm border border-white p-1 text-black">
-      {keyboard.toUpperCase()}
-    </button>
-  ));
+  const keyboardButtons = alphabet.map((keyboard) => {
+    const isGuessed = usersLetters.includes(keyboard);
+    const isCorrect =
+      usersLetters.includes(keyboard) && currentWord.includes(keyboard);
+    const isWrong = isGuessed && !isCorrect;
+
+    const keyboardClass = clsx(
+      "size-8 rounded-sm border border-white p-1 text-black",
+      isCorrect ? "bg-green-400" : isWrong ? "bg-red-500" : "bg-yellow-500",
+    );
+
+    return (
+      <button onClick={() => addLetter(keyboard)} className={keyboardClass}>
+        {keyboard.toUpperCase()}
+      </button>
+    );
+  });
 
   const displayWord = currentWord
     .split("")
     .map((letter) => (
-      <span className="span-square">{letter.toUpperCase()}</span>
+      <span className="span-square">
+        {usersLetters.includes(letter) ? letter.toUpperCase() : ""}
+      </span>
     ));
 
-  const displayLanguages = languages.map(({ name, backgroundColor, color }) => (
-    <div
-      style={{ backgroundColor: backgroundColor, color: color }}
-      className="w-fit rounded-sm p-1"
-    >
-      {name}
-    </div>
-  ));
+  const displayLanguages = languages.map(
+    ({ name, backgroundColor, color }, index) => (
+      <div
+        style={{ backgroundColor: backgroundColor, color: color }}
+        className={`w-fit rounded-sm p-1 ${index < wrongGuessCount && "opacity-75 grayscale"}`}
+      >
+        {name}
+      </div>
+    ),
+  );
 
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center gap-5 bg-[#282726] px-5 py-10 text-center">
